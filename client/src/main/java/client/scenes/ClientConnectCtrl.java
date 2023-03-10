575ed997ff8f188cc164bbb9ac77449b45e54ff8
Bogdan Luca Paramon <b.l.paramon@student.tudelft.nl>
2023-03-10 17:15:40 +0100

    Added images to the right folder, slightly modified design and added javadoc

--- client/src/main/java/client/scenes/ClientConnectCtrl.java
+++ client/src/main/java/client/scenes/ClientConnectCtrl.java
@@ -8,12 +8,20 @@ public class ClientConnectCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
+    /**
+     * Setup server and main controller
+     * @param server server to connect to
+     * @param mainCtrl the main controller - for switching scenes
+     */
     @Inject
     public ClientConnectCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
 
+    /**
+     * Uses showHome method to switch scenes to Home scene
+     */
     public void switchSceneToHome() {
         mainCtrl.showHome();
     }
