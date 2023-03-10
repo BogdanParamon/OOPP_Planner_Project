26661b6c396352076cc6d3dfe015d24816d62eba
Filip Čajági <f.cajagi@student.tudelft.nl>
2023-03-10 18:51:48 +0100

    Fixed checkstyle and added javadoc for database entity classes

--- commons/src/main/java/commons/Task.java
+++ commons/src/main/java/commons/Task.java
@@ -18,27 +18,54 @@ public class Task {
     @JoinColumn(name = "listId")
     public TaskList list;
 
+    /**
+     * Creates a new Task object with the given title and list.
+     *
+     * @param title The title to be given to the Task
+     * @param list The TaskList in which this Task will be
+     */
     public Task(String title, TaskList list) {
         this.title = title;
         this.list = list;
     }
 
+    @SuppressWarnings("unused")
     private Task() {
+        // For object mapper
     }
 
+    /**
+     * Compares this object to another object and returns true only if they are equal.
+     * Two Tasks are equal if they have the same taskId, title and are in the same list.
+     *
+     * @param o The object that is going to be compared to this
+     * @return True if and only if the other object is a Task with the same attributes
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return taskId == task.taskId && Objects.equals(title, task.title) && Objects.equals(list.listId, task.list.listId);
+        return taskId == task.taskId && Objects.equals(title, task.title)
+                && Objects.equals(list.listId, task.list.listId);
     }
 
+    /**
+     * Generates a hash code for this object,
+     * which uses the taskId, title and list attributes.
+     *
+     * @return The generated hash code for this object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(taskId, title, list.listId);
     }
 
+    /**
+     * Generates a String representation of this object and its attributes in a multi-line style.
+     *
+     * @return The generated String representation for this object
+     */
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
--- commons/src/main/java/commons/TaskList.java
+++ commons/src/main/java/commons/TaskList.java
@@ -23,32 +23,66 @@ public class TaskList {
     @OneToMany(mappedBy = "list")
     public Set<Task> tasks;
 
+    /**
+     * Creates a new TaskList object with the given title, board and an empty set of Tasks.
+     *
+     * @param title The title to be given to the TaskList
+     * @param board The Board on which this TaskList will be
+     */
     public TaskList(String title, Board board) {
         this.title = title;
         this.board = board;
-        tasks = new HashSet<Task>();
+        tasks = new HashSet<>();
     }
 
+    @SuppressWarnings("unused")
     private TaskList() {
+        // For object mapper
     }
 
+    /**
+     * Compares this object to another object and returns true only if they are equal.
+     * Two TaskLists are equal if they have the same listId, title, set of tasks
+     * and are on the same Board.
+     *
+     * @param o The object that is going to be compared to this
+     * @return True if and only if the other object is a TaskList with the same attributes
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TaskList taskList = (TaskList) o;
-        return listId == taskList.listId && Objects.equals(title, taskList.title) && Objects.equals(board.boardId, taskList.board.boardId) && Objects.equals(tasks, taskList.tasks);
+        return listId == taskList.listId && Objects.equals(title, taskList.title)
+                && Objects.equals(board.boardId, taskList.board.boardId)
+                && Objects.equals(tasks, taskList.tasks);
     }
 
+    /**
+     * Generates a hash code for this object,
+     * which uses the listId, title, board and tasks attributes.
+     *
+     * @return The generated hash code for this object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(listId, title, board.boardId, tasks);
     }
 
-    public void addTask(Task task){
+    /**
+     * Adds a Task to the set of all Tasks in this TaskList.
+     *
+     * @param task The Task to be added to this TaskList
+     */
+    public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Generates a String representation of this object and its attributes in a multi-line style.
+     *
+     * @return The generated String representation for this object
+     */
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
