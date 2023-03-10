2857af9a23c885a260cb737163fbc4d2f6cdbd31
Serban Mihai Radu <M.R.Serban-1@student.tudelft.nl>
2023-03-10 17:02:04 +0100

    added javadoc to BoardController class

--- server/src/test/java/server/api/TestTaskRepository.java
+++ server/src/test/java/server/api/TestTaskRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class TaskRepositoryTest implements TaskRepository {
+public class TestTaskRepository implements TaskRepository {
 
     public final List<Task> quotes = new ArrayList<>();
     public final List<String> calledMethods = new ArrayList<>();
