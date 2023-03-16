e93a7e4b679ace10eb714de5df0bfa5f1c0bb050
Filip Čajági <f.cajagi@student.tudelft.nl>
2023-03-16 16:20:38 +0100

    Fixed tests for database entity classes

--- commons/src/test/java/commons/TaskTest.java
+++ commons/src/test/java/commons/TaskTest.java
@@ -6,37 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TaskTest {
-    private static final Board SOME_BOARD = new Board("BoardTitle");
-    private static final TaskList SOME_LIST = new TaskList("ListTitle", SOME_BOARD);
+    //private static final Board SOME_BOARD = new Board("BoardTitle");
+    //private static final TaskList SOME_LIST = new TaskList("ListTitle", SOME_BOARD);
 
     @Test
     public void checkConstructor() {
-        var t = new Task("Task Title", SOME_LIST);
+        var t = new Task("Task Title");
         assertEquals("Task Title", t.title);
-        assertEquals(SOME_LIST, t.list);
     }
 
     @Test
     public void equalsHashCode() {
-        var a = new Task("a", SOME_LIST);
-        var b = new Task("a", SOME_LIST);
+        var a = new Task("a");
+        var b = new Task("a");
         assertEquals(a, b);
         assertEquals(a.hashCode(), b.hashCode());
     }
 
     @Test
     public void notEqualsHashCode() {
-        var a = new Task("a", SOME_LIST);
-        var b = new Task("b", SOME_LIST);
+        var a = new Task("a");
+        var b = new Task("b");
         assertNotEquals(a, b);
         assertNotEquals(a.hashCode(), b.hashCode());
     }
 
     @Test
     public void hasToString() {
-        var actual = new Task("a", SOME_LIST).toString();
+        var actual = new Task("a").toString();
         assertTrue(actual.contains(Task.class.getSimpleName()));
         assertTrue(actual.contains("\n"));
-        assertTrue(actual.contains("list"));
+        assertTrue(actual.contains("title"));
     }
 }
