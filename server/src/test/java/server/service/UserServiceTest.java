32a0c0a577be25898f9f2f84e493eb5a6bbef3d2
Suruo Zhang <S.Zhang-39@student.tudelft.nl>
2023-04-11 18:01:40 +0200

    Added more tests for Service

--- server/src/test/java/server/service/UserServiceTest.java
+++ server/src/test/java/server/service/UserServiceTest.java
@@ -71,4 +71,58 @@ public class UserServiceTest {
         assertEquals(board1.title, result.get(board1.boardId));
         assertEquals(board2.title, result.get(board2.boardId));
     }
+
+    @Test
+    public void testVerifyAdminPassword() {
+        String adminPassword = "admin";
+        assertTrue(userService.verifyAdminPassword(adminPassword));
+    }
+
+    @Test
+    public void testLeaveBoard() {
+        User user = new User("testUser");
+        user.userId = 1l;
+
+        Board board1 = new Board("Board 1");
+        board1.setId(1L);
+        Board board2 = new Board("Board 2");
+        board2.setId(2L);
+
+        user.boards.add(board1);
+        user.boards.add(board2);
+
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(user);
+
+        String result = userService.leaveBoard(1L, 1L);
+
+        assertEquals("Successful", result);
+        assertEquals(1, user.boards.size());
+        assertTrue(user.boards.contains(board2));
+    }
+
+    @Test
+    public void testLeaveMessage() {
+        User user = new User("testUser");
+        user.userId = 1l;
+
+        Board board1 = new Board("Board 1");
+        board1.setId(1L);
+        Board board2 = new Board("Board 2");
+        board2.setId(2L);
+
+        user.boards.add(board1);
+        user.boards.add(board2);
+
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(user);
+
+        Long result = userService.leaveMessage(1L, 1L);
+
+        assertEquals(1L, result);
+        assertEquals(1, user.boards.size());
+        assertTrue(user.boards.contains(board2));
+    }
 }
